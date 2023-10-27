256600f8a94410d8de95382d1c69d22ea227a2da
11999977 <boukoussoudahel@gmail.com>
2023-10-27 11:49:37 +0100

    premier commit

---
+++ Gestiondesapp/src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const title = "Bonjour Manguimissou"
+function App() {
+  return  <>
+            <h1 id='title' className='title'>{title}</h1>
+          </>
+}
+
+export default App
